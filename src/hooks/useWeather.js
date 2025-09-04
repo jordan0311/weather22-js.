5508d1f05b84f9bb86f242f83b10b7374bc325e0
jordanRomero <220813255+jordan0311@users.noreply.github.com>
2025-09-03 21:09:21 -0500

    aplicion del clima

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
 const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
 export function useWeather() {
